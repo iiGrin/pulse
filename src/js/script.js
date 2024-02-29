2d86a60ec6b1b2073770b7ea1ce80169e8a397bb
devIggy <devdevgrin@gmail.com>
2024-02-29 11:31:02 +0300

    feat: carousel & 575px adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,40 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    hamburger = document.querySelector('.hamburger'),
-    logoMobile = document.querySelector('.logo-mobile');
+window.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector(".menu"),
+    menuLink = document.querySelectorAll(".menu_link"),
+    hamburger = document.querySelector(".hamburger");
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("hamburger_active");
+    menu.classList.toggle("menu_active");
+  });
+
+  menuLink.forEach((item) => {
+    item.addEventListener("click", () => {
+      hamburger.classList.toggle("hamburger_active");
+      menu.classList.toggle("menu_active");
     });
+  });
+});
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+$(document).ready(function () {
+  $(".carousel__inner").slick(
+    {
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"/></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"/></button>',
+        responsive:
+        [
+            {
+              breakpoint: 992, 
+              settings: {
+                dots: false,
+                arrows: false
+              },
+              breakpoint: 600,
+              breakpoint: 480,
+              breakpoint: 320,
+            }
+          ]
+      }
+  );
+});
